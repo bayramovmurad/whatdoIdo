5755bd9718590a3ec88f2c19ea71451d005e6ad2
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-03-22 22:28:06 +0400

    Necessary arrangements were made and the first message was sent to the database.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
-const Home = () => {
+import { PrismaClient } from "@prisma/client";
+
+import AddTodo from "@/components/shared/AddTodo";
+
+
+const Home = async () => {
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <span className="text-3xl font-extrabold uppercase">
+        What Do I Do
+      </span>
+
+      <div className="flex justify-center flex-col items-center w-[1000px] ">
+        <AddTodo />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
